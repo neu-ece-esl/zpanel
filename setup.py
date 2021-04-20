ba8cd0e0a79244bb248753ce330669610177abf2
Bruno <brunosmmm@gmail.com>
2021-04-20 14:56:23 -0400

    Put requirements in setup.py and refactor

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='zpanel',
-    version='0.1',
+    name="zpanel",
+    version="0.1",
     packages=find_packages(),
-    package_dir={'': '.'},
-    package_data={'zmon': ['data/views/*', 'data/web_static/*']},
-
-    install_requires=[],
-
+    package_dir={"": "."},
+    package_data={"zmon": ["data/views/*", "data/web_static/*"]},
+    install_requires=[
+        "pyzmq",
+        "cheroot",
+        "bottle @ git+https://github.com/bottlepy/bottle.git@master#egg=bottle",
+    ],
     author="Bruno Morais",
     author_email="brunosmmm@gmail.com",
     description="Zpanel Zedboard IO emulator",
-
-    scripts=['zpanel'],
+    scripts=["zpanel"],
 )
